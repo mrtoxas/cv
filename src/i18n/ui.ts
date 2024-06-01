4e5be634b999d8d42d1fee4f37d509d4256aa616
mtoxas <mtoxas@gmail.com>
2024-06-01 18:28:28 +0300

    translate ui

---
+++ src/i18n/ui.ts
@@ -0,0 +1,40 @@
+export type Lang = 'en' | 'ua' | 'ru';
+
+export const defaultLang: Lang = 'en';
+
+type Ui = {
+ [key in Lang]: Record<string, string>;
+};
+
+export const ui:Ui = {
+  en: {
+    'page.meta.title': 'Portfolio: Anton Bojko',
+    'page.meta.description': 'Professional developer portfolio',
+    'bar.downloadBtn.title': 'Download PDF version',
+    'bar.printBtn.title': 'Print this page',
+    'bar.themeButton.title': 'Dark/Light mode',
+  },
+  ua: {
+    'page.meta.title': 'Portfolio: Anton Bojko',
+    'page.meta.description': 'Professional developer portfolio',
+    'bar.downloadBtn.title': 'Завантажити PDF-версiю',
+    'bar.printBtn.title': 'Роздрукувати цю сторiнку',
+    'bar.themeButton.title': 'Темна/Свiтла тема',
+  },
+  ru: {
+    'page.meta.title': 'Portfolio: Anton Bojko',
+    'page.meta.description': 'Professional developer portfolio',
+    'bar.downloadBtn.title': 'Скачать PDF-версию',
+    'bar.printBtn.title': 'Распечатать эту страницу',
+    'bar.themeButton.title': 'Тёмная/Светлая тема',
+  },
+} as const;
+
+export const formatLangCode = (code: string) => {
+  return code.split('-')[0].toLowerCase() as Lang;
+}
+
+export const useTranslations = (lang?: string) => {
+  const t = (key: string) => ui[lang ? formatLangCode(lang) : defaultLang][key];
+  return t;
+}
