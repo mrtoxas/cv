ea331454d6b5f762c4e6f361192d41e5a2ba257c
mtoxas <mtoxas@gmail.com>
2024-08-09 13:43:50 +0300

    gallery, navbar draft

--- src/content/config.ts
+++ src/content/config.ts
@@ -79,6 +79,7 @@ const mainCollection = defineCollection({
             name: stringTrim,
             link: stringTrim,
           })),
+          images: z.array(stringTrim).optional()
         })
       )
     }),
